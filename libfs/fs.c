efeb4e83d9d2e7204b82dc030ae9425fbd5bd64b
ekbostan <114015851+ekbostan@users.noreply.github.com>
2022-11-06 15:15:42 -0800

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -11,7 +11,24 @@
 
 int fs_mount(const char *diskname)
 {
-	/* TODO: Phase 1 */
+	//Check if the disk can be openned
+        if(block_disk_open(diskname) == -1 ){
+                return -1;
+        }
+        //Check if block is readable
+        if(block_read(0,(void*)super_block) == -1){
+                return -1;
+        }
+        //Check number of blocks and signature
+        if(block_disck_count() != super_block.total_num_block && memcp(super_block.sign,"ECS150FS",64) != 0){
+                return -1;
+        }
+        //Initialize memmory for flat array
+        flat_array = malloc((super_block.num_FAT_blocks) * 4096 * sizeof(unint16_t));
+        //Check if memmory is initialized correctly
+        if(flat_array == NULL){
+                return -1
+        }
 }
 
 int fs_umount(void)
