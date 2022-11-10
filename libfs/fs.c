926f889f8b1e406241facf76b00770ea6df24b8e
ekbostan <114015851+ekbostan@users.noreply.github.com>
2022-11-09 19:55:42 -0800

    Update fs.c
    This prints out the info part right however we are missing flat array ratio which I commented in the info function.

--- libfs/fs.c
+++ libfs/fs.c
@@ -9,86 +9,78 @@
 
 struct __attribute__ ((__packed__)) superblock{
     char sign[8];
-    uint16_t total_num_blocks, num_data_blocks, root_dir_idx, data_init_idx;
+    uint16_t total_num_blocks;
+    uint16_t root_dir_idx;
+    uint16_t data_init_idx;
+    uint16_t num_data_blocks;
     uint8_t num_FAT_blocks;
     uint8_t padding[4079];
 };
-
 struct __attribute__ ((__packed__)) FAT{
     uint16_t data_block; //0 if free
-    struct FAT* next;
 };
-
 struct __attribute__ ((__packed__)) rootdir{
-    uint16_t file_name[8];
+    char file_name[16];
     uint32_t file_size;
     uint16_t idx;
     uint8_t padding[10];
 };
 struct superblock* super_block;
 struct FAT* flat_array;
-struct rootdir* rootdir_array[128];
+struct rootdir* rootdir_array;
+char signature[8]="ECS150FS";
 
 int fs_mount(const char *diskname)
-{
-	
-	//Check if the disk can be openned
-        if(block_disk_open(diskname) == -1 ){
-                return -1;
-        }
-        //Check if block is readable
-        if(block_read(0,(void*)super_block) == -1){
-                return -1;
-        }
-        //Check number of blocks and signature
-        if(block_disck_count() != super_block->total_num_block && memcmp(super_block->sign,"ECS150FS",8) != 0){
-                return -1;
-        }
-        //Initialize memmory for flat array
-        flat_array = malloc((super_block->num_FAT_blocks) * 4096 * sizeof(unint16_t));
-        //Check if memmory is initialized correctly
-        if(flat_array == NULL){
-                return -1
-        }
-        //Check flat array size and number of blocks 
-        if(super_block->total_num_block > (super_block->num_FAT_blocks)* 2048){
-                 return -1;
-          }
-        //Read the data
-         for (int i = 0; i < super_block->num_FAT_blocks;i++ )
-        {
-                 if(block_read(i+1, &flat_array[i*2048]) == -1){
+{ 
+	if(block_disk_open(diskname) == -1){
+            return -1;
+	}
+	super_block = malloc(4096);
+	rootdir_array = malloc(4096);
+	if(block_read(0, (void*)super_block) == -1){
+			return -1;
+	}
+	if(block_disk_count() != super_block->total_num_blocks){
+		return -1;
+	}
+	if(memcmp(super_block->sign,"ECS150FS",8) != 0){
+		return -1;
+	}
+	flat_array = malloc((super_block->num_FAT_blocks) * 4096 * sizeof(uint16_t));
+	if(flat_array == NULL){
+			return -1;
+	}
+	for (int i = 0; i < super_block->num_FAT_blocks;i++){
+		if(block_read(i+1, &flat_array[i*2048]) == -1){
 
-                         return -1;
-                 }
-         }
-        //Check root dir
-        if(block_read(super_block->root_dir_idx,&rootdir_array) == -1){
-        return -1;
-        }
+			return -1;
+		}
+		
+	}
+	if(block_read(super_block->root_dir_idx,(void*)rootdir_array) == -1){
+			return -1;
+   	}
 
-        return 0;
+    return 0;
 
-	
 }
 
 int fs_umount(void)
-{
-	//Check if the superblock is empty
+{	
 	if(super_block == NULL){
          return -1;
 
         }
 	//Check
-        if(block_Write(0,(void*)&super_block) == -1 ||block_write(super_block->root_dir_idx,&rootdir_array == -1 )){
+        if(block_write(0,(void*)super_block) == -1 ||block_write(super_block->root_dir_idx,rootdir_array) == -1){
                 return -1;
-
         }
-        if(super_block->total_num_block > (super_block->num_FAT_blocks)* 2048){
-                 return -1;
-         }
-        for (int i = 0; i < super_block->num_FAT_blocks;i++ )
-        {
+	
+    //    if(super_block->total_num_blocks != (super_block->num_FAT_blocks* 2048)){
+      //           return -1;
+  //       }
+	
+        for (int i = 0; i < super_block->num_FAT_blocks;i++){
                  if(block_read(i+1, &flat_array[i*2048]) != -1){
 
                          continue;
@@ -96,74 +88,113 @@ int fs_umount(void)
                  else
                          return -1;
         }
-        int bit_count = 0
-        while(bit_count < 128){
-                memset(rootdir_array[bit_count]->file_name,0,strlen(rootdir_array[bit_count]->file_name))
-                rootdir_array[bit_count]->idx =0;
-                rootdir_array[bit_count]->file_Size =0;
-                bit_count++;
-        }
+
+        memset(rootdir_array->file_name,0,16);
+        rootdir_array->idx =0;
+        rootdir_array->file_size =0;
         free(flat_array);
         super_block->total_num_blocks =0;
         super_block->num_data_blocks = 0;
         super_block->root_dir_idx =0;
         super_block->data_init_idx=0;
-        super_block->numt_FAT_blocks =0;
-        memset(super_block->signature,'\0',8);
-        if(block_disk_close == -1){
+        super_block->num_FAT_blocks =0;
+        memset(super_block->sign,'\0',8);
+        if(block_disk_close() == -1){
                 return -1;
         }
-
-
-        return 0;
+	return 0;
 }
+
 
 int fs_info(void)
 {
-	/* TODO: Phase 1 */
+    printf("FS Info:\n");
+    printf("total_blk_count=%d\n",super_block->total_num_blocks);
+    printf("fat_blk_count=%d\n",super_block->num_FAT_blocks);
+    printf("rdir_blk=%d\n",super_block->root_dir_idx);
+    printf("data_blk=%d\n",super_block->data_init_idx);
+    printf("data_blk_count=%d\n",super_block->num_data_blocks);
+	
+  int while_counter = 0;
+
+    // THIS PART IS THE ONLY PART WE ARE MISSING
+    // Cuz we are getting you can not compare a struct with an integer at the line flat_array[i] == 0
+    /*int fat_counter = 0;
+     int while_counter = 0;
+ 	while(while_counter < super_block->total_num_blocks)
+	{
+		if(flat_array[i] == 0)
+		{
+			fat_counter = fat_counter + 1;
+		}
+	}
+
+  	printf("fat_free_ratio=%d/%d\n", fat_counter, super_block->total_num_blocks);
+  */
+	int root_num = 0;
+	while_counter = 0;
+	while(while_counter<128) 
+	{
+		if(rootdir_array->file_size == 0)
+		{
+			root_num = root_num + 1;
+		}
+		while_counter++;
+	} 
+	printf("rdir_free_ratio=%d/%d\n", root_num,128);
+	return 0;
 }
 
 int fs_create(const char *filename)
 {
-	/* TODO: Phase 2 */
+	
+	return 0;
 }
 
 int fs_delete(const char *filename)
 {
 	/* TODO: Phase 2 */
+	return 0;
 }
 
 int fs_ls(void)
 {
 	/* TODO: Phase 2 */
+	return 0;
 }
 
 int fs_open(const char *filename)
 {
 	/* TODO: Phase 3 */
+	return 0;
 }
 
 int fs_close(int fd)
 {
 	/* TODO: Phase 3 */
+	return 0;
 }
 
 int fs_stat(int fd)
 {
 	/* TODO: Phase 3 */
+	return 0;
 }
 
 int fs_lseek(int fd, size_t offset)
 {
 	/* TODO: Phase 3 */
+	return 0;
 }
 
 int fs_write(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
+	return 0;
 }
 
 int fs_read(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
+	return 0;
 }
